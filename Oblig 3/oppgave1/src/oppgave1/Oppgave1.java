83226751dad7c690e6f366f6d3cd11d55bdeddef
Selma Karagøz <115637278+Selmakrgzz@users.noreply.github.com>
2023-04-06 13:17:45 +0200

    Delete Oblig 3 directory

--- Oblig 3/oppgave1/src/oppgave1/Oppgave1.java
+++
@@ -1,8 +0,0 @@
-package oppgave1;
-
-public class Oppgave1 {
-    public static void main(String[] args) {
-        UnikeTall ny = new UnikeTall(65);
-        ny.skrivUt();
-    }
-}
--- Oblig 3/oppgave1_1/src/oppg1/main.java
+++
@@ -1,8 +0,0 @@
-package oppg1;
-
-public class main {
-    public static void main(String[] args) {
-        UnikeTall ny = new UnikeTall(40);
-        ny.skrivUt();
-    }
-}
