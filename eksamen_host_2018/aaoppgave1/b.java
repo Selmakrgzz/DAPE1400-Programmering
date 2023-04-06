f90ef5934d06941dfacc1afe52f5c8f902aa78b2
Selma Karagøz <115637278+Selmakrgzz@users.noreply.github.com>
2023-04-06 13:21:46 +0200

    Delete eksamen_host_2018 directory

--- eksamen_host_2018/aaoppgave1/b.java
+++
@@ -1,9 +0,0 @@
-package eksamen_host_2018.aaoppgave1;
-
-public class b {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 25; i++){
-            System.out.println(i);
-        }
-    }
-}
--- eksamen_host_2018/oppgave1/b_oppgave1.java
+++
@@ -1,9 +0,0 @@
-package eksamen_host_2018.oppgave1;
-
-public class b_oppgave1 {
-    public static void main(String [] args){
-        for (int i = 1; i <= 25; i++){
-            System.out.println(i);
-        }
-    }
-}
