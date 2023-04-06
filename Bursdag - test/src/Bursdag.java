022392d98e6799e847e28010503e4b6278a31485
Selma Karagøz <115637278+Selmakrgzz@users.noreply.github.com>
2023-04-06 13:01:59 +0200

    Add files via upload

---
+++ Bursdag - test/src/Bursdag.java
@@ -0,0 +1,5 @@
+public class Bursdag {
+    public static void main (String [] args) {
+        System.out.println("Bursdag");
+    }
+}
---
+++ briller/src/briller.java
@@ -0,0 +1,5 @@
+public class briller {
+    public static void main (String [] args){
+        System.out.println("briller");
+    }
+}
